0a909fe004e9e85fff8570e4d6a4b7e08bbdbe85
dldudgjs31 <dldudgjs31@naver.com>
2022-01-04 09:54:08 +0900

    (코테문제풀이)

---
+++ commit.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+/root/bin/git config --global user.name dldudgjs31
+/root/bin/git config --global user.email dldudgjs31@naver.com
+/root/bin/git config --global user.mail dldudgjs31@naver.com
+/root/bin/git config --list > /root/silk/python-for-coding-test/git_id_check
+
+cd /root/silk/python-for-coding-test/
+
+/root/bin/git add .
+
+/root/bin/git checkout -b main
+
+/root/bin/git add .
+
+eval $(ssh-agent -s)
+
+ssh-add ~/.ssh/lee
+
+/root/bin/git commit -am "(코테문제풀이) $1" 
+
+/root/bin/git push --set-upstream origin main
+
